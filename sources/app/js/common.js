5dfe2d7a5d10b3ec162c0e7c131f68cfe4f0cdcc
vitalikpetreniuk <petreniuk.ua@gmail.com>
2017-10-20 21:37:11 +0300

    Добавил папку с исходниками sources

---
+++ sources/app/js/common.js
@@ -0,0 +1,86 @@
+$(function() {
+
+
+    $('.popup-modal').magnificPopup({
+        type: 'inline',
+        modal: true,
+        closeOnBgClick: true
+    });
+    $(document).on('click', '.popup-modal-dismiss', function (e) {
+        e.preventDefault();
+        $.magnificPopup.close();
+    });
+
+    $('.cg-scroll').slimScroll({
+        railVisible: true,
+        alwaysVisible: true,
+        color: '#0a1f3c',
+        railColor: '#b4bbc5',
+        railOpacity: 1,
+        size: '4px',
+        allowPageScroll: true
+    });
+
+    $('.cg-drop-block > button').on('click', function(){
+        $(this).toggleClass('open');
+        $(this).next('div').toggle('fast');
+    });
+
+    $('.cg-btn-scroll-bottom').on('click' ,function(){
+        scrolled=scrolled+100;
+
+        $('.cg-projects-list').animate({
+            scrollTop:  scrolled
+        });
+
+    });
+
+    $('.cg-tooltip-button').on('click', function(){
+        $(this).siblings('.cg-tooltip').toggleClass('active');
+    });
+
+    $('.cg-tooltip > button').on('click', function(){
+        $(this).closest('.cg-tooltip').removeClass('active');
+    });
+
+    $('.cg-modal-channel > div > button').on('click', function () {
+        $(this).toggleClass('open');
+        $(this).closest('.cg-modal-channel').find('.cg-modal-channel-drop').toggle('fast');
+    });
+
+    $('.cg-btn-panel-hide').on('click', function () {
+        $(this).toggleClass('active');
+        $('.cg-cl-left').toggleClass('inactive');
+        $('.cg-cl-right').toggleClass('fwidth');
+    });
+
+    $('.cg-sb-button').on('click', function () {
+        $(this).toggleClass('active');
+        $(this).closest('.cg-segment-block').toggleClass('open');
+        $(this).closest('.cg-segment-block').find('.cg-sb-cont-in').toggle('fast');
+    });
+
+    $('.cg-segments-buttons button').on('click', function () {
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.cg-segments-buttons button:first-child').on('click', function () {
+        $('.cg-segments input').each(function () {
+            if(!$(this).is(':checked'))
+            {
+                $(this).trigger('click');
+            }
+        });
+    });
+
+    $('.cg-segments-buttons button:last-child').on('click', function () {
+        $('.cg-segments input').each(function () {
+            if($(this).is(':checked'))
+            {
+                $(this).trigger('click');
+            }
+        });
+    });
+
+});
